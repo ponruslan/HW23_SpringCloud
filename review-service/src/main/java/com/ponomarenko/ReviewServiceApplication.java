5f41ddb1c2f4775ee9a468f62e4971a24aa2979d
Ruslan Ponomarenko <panruslan@gmail.com>
2020-09-19 19:17:10 +0300

    add docker

--- review-service/src/main/java/com/ponomarenko/ReviewServiceApplication.java
+++ review-service/src/main/java/com/ponomarenko/ReviewServiceApplication.java
@@ -3,11 +3,12 @@ package com.ponomarenko;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class ReviewServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReviewServiceApplication.class, args);
